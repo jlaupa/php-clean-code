69ba9dbccae81ec845efe38ab61b958515eab673
Jairo Laupa <jlaupa@blended.com.ar>
2021-07-03 11:09:10 +0200

    refactor: GildedRose:updateQuality

--- src/Item.php
+++ src/Item.php
@@ -2,7 +2,11 @@
 
 namespace Runroom\GildedRose;
 
-class Item {
+class Item
+{
+    public const AGED = 'Aged Brie';
+    public const BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
+    public const SULFURAS = 'Sulfuras, Hand of Ragnaros';
 
     /**
      * @var string $name
@@ -32,12 +36,64 @@ class Item {
         $this->quality = $quality;
     }
 
-    /**
-     * @return string
-     */
-    public function __toString(): string
+    public function qualityBackstageRules(): void
     {
-        return "{$this->name}, {$this->sell_in}, {$this->quality}";
+        if (($this->name === self::BACKSTAGE) && !$this->qualityClean($this->sell_in)) {
+            $this->qualityIncrease(3);
+            if ($this->sell_in > 5) {
+                $this->qualityDecrease();
+                if ($this->sell_in > 10) {
+                    $this->qualityDecrease();
+                }
+            }
+        }
     }
 
+    public function qualityAgeRules(): void
+    {
+        if (($this->name === self::AGED) && $this->quality < 50) {
+            $this->qualityIncrease(2);
+        }
+    }
+
+    public function qualityOtherRules(): void
+    {
+        if ($this->name !== self::BACKSTAGE && $this->name !== self::AGED) {
+            $this->qualityDecrease();
+            if ($this->sell_in <= 0) {
+                $this->qualityDecrease();
+                $this->qualityClean($this->quality);
+            }
+        }
+    }
+
+    /**
+     * @param int $value
+     * @return bool
+     */
+    public function qualityClean(int $value): bool
+    {
+        $condition = $value <= 0;
+        if ($condition) {
+            $this->quality = 0;
+        }
+
+        return $condition;
+    }
+
+    /**
+     * @param int $value
+     */
+    public function qualityIncrease(int $value = 1): void
+    {
+        $this->quality += $value;
+    }
+
+    /**
+     * @param int $value
+     */
+    public function qualityDecrease(int $value = 1): void
+    {
+        $this->quality -= $value ;
+    }
 }
